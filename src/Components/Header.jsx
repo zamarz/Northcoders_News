d9553027a644b4c9f7b1a3cb7b92e84eb18625e3
zamarz <zachmarzouk@gmail.com>
2023-09-06 08:57:32 +0100

    "Adding article vote functionality"

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,18 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { useContext } from "react";
+import { UserContext } from "../contexts/Users";
 
 const Header = () => {
+  const { username } = useContext(UserContext);
+
   return (
     <Container>
       <Row className="justify-content-md-center">
         <Col>
           <h1 className="display-2">Northcoders News</h1>
+          <p>Logged in as {username}</p>
         </Col>
       </Row>
     </Container>
