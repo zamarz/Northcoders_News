b7b7611d3ae3899153b20a8447d2344d76ec1ae6
zamarz <zachmarzouk@gmail.com>
2023-09-08 09:47:52 +0100

    "Added a delete comment to each comment"

--- src/Components/CommentCard.jsx
+++ src/Components/CommentCard.jsx
@@ -1,8 +1,34 @@
 import Card from "react-bootstrap/Card";
-import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
+import { useState } from "react";
+import { useContext } from "react";
+import { UserContext } from "../contexts/Users";
+import { deleteArticleComment } from "../utls/api";
+import Alert from "react-bootstrap/Alert";
 
 const CommentCard = ({ comment }) => {
+  const [disabledButton, setDisabledButton] = useState(false);
+  const [showComment, setShowComment] = useState(false);
+
+  const { username } = useContext(UserContext);
+
+  // if (username === comment.author) {
+  //   setDisabledButton(false);
+  // }
+
+  // in line way to do it: username === comment.author ? false : true
+
+  //needs to go into button class disabled={disabledButton}
+  //also do the button loading thing once it is deleting or something
+  //also, just do it the simple way first
+
+  // useEffect(
+  //   (event) => {
+  //     event.preventDefault();
+  //   },
+  //   [commentID]
+  // );
+
   return (
     <li>
       <Card border="primary" style={{ width: "18rem" }} />
@@ -11,6 +37,29 @@ const CommentCard = ({ comment }) => {
         <Card.Subtitle>Author: {comment.author}</Card.Subtitle>
         <Card.Text>Votes: {comment.votes}</Card.Text>
         <Card.Text>Published: {comment.created_at}</Card.Text>
+        <Button
+          disabled={username === comment.author ? disabledButton : true}
+          type="submit"
+          variant="warning"
+          onClick={() => {
+            deleteArticleComment(comment.comment_id);
+            setShowComment(true);
+            setDisabledButton(true);
+          }}
+        >
+          Delete comment
+        </Button>
+        <Alert show={showComment} variant="success">
+          <Alert.Heading>Your comment has been deleted</Alert.Heading>
+          <div>
+            <Button
+              onClick={() => setShowComment(false)}
+              variant="outline-success"
+            >
+              Close
+            </Button>
+          </div>
+        </Alert>
       </Card.Body>
     </li>
   );
